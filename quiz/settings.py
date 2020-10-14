5446bf5bfda1b2d541e71b3e54d371d5d30357ee
SpeeritX <jedrek.185@gmail.com>
2020-10-15 01:07:01 +0200

    Added static files

--- quiz/settings.py
+++ quiz/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import mimetypes
-
-
-mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Channels
 ASGI_APPLICATION = 'quiz.routing.application'
